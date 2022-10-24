a4b7a9b6b38139e9d4a712d4cf93175ba1d0b849
Echo <103549130+Sp1d3rH3X@users.noreply.github.com>
2022-10-23 21:46:23 -0400

    Create teacherMain.java

---
+++ Teacher/teacherMain.java
@@ -0,0 +1,13 @@
+class teacherMain {
+    public static void main(String[] args)
+    {
+        teacher t1 = new teacher("Tommy", 36, "Computer Science A", 356.99);
+        System.out.println(t1);
+
+        teacher t2 = new teacher("Steven", 41, "Intigrated Science",600000.00);
+        System.out.println(t2);
+
+        teacher t3 = new teacher("Stanly" , 57, "English" , 300.00);
+        System.out.println(t3);
+    }
+}
