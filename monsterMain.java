061655028824a2cd07c852810db6fdf3d22e495d
Echo <103549130+Sp1d3rH3X@users.noreply.github.com>
2022-10-23 21:40:04 -0400

    Delete monsterMain.java

--- monsterMain.java
+++
@@ -1,9 +0,0 @@
-class monsterMain {
-  public static void main(String[] args) {
-    monster m1 = new monster(); // uses default / building monster
-    System.out.println(m1);     //printing monster
-    monster m2 = new monster(true, 8, "spider"); //uses overloaded
-    System.out.println(m2);
-    System.out.println(m2.getEyes()); //gets the amount of eyes of monster 2 and prints it with system out print line.
-  }
-}
